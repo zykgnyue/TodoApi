18100f8ebe6731c38b5fc3c29ae5cad90f04a8c0
git <git@192.168.1.109>
2018-04-06 14:09:27 +0800

    添加项目文件。

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Mmodels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{Id}",Name="GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if(item==null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if(item==null)
+            {
+                return BadRequest();
+            }
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        /// <summary>
+        /// Update method: HTTTP put
+        /// </summary>
+        /// <param name="id">Id is from URL </param>
+        /// <param name="item">from post body </param>
+        /// <returns>204 no content</returns>
+        [HttpPut("{id}")]
+        public IActionResult Update(long id ,[FromBody] TodoItem item)
+        {
+           
+            if(item ==null || item.Id!=id)
+            {
+                return BadRequest();
+            }
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if(todo ==null)
+            {
+                return NotFound();
+            }
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if(todo ==null)
+            {
+                return NotFound();
+            }
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            //responses:204 
+            return new NoContentResult();
+        }
+    }
+}
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Mmodels;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
